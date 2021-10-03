c899e86421a2bbd5c03f6be63fc8d73f94af1d76
Karol05 <201219@ids.upchiapas.edu.mx>
2021-10-03 04:16:43 -0500

    Tercer commit

---
+++ app/page/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class App extends React.Component{
+    render() {
+        return(
+            <React.Fragment>
+                <Header>
+                    <Main>
+
+                    </Main>
+                </Header>
+            </React.Fragment>
+        )
+    }
+
+}
+export default App;
---
+++ app/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+        publicPath: "/public"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-react"],
+                        plugins: [
+                            "react-hot-loader/babel",
+                            "jsx-control-statements"
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'assets/images/[name].[ext]'
+                        }
+
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        port: 8081,
+    }
+};
